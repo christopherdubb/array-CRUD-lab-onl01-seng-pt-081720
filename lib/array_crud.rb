b9749fd99b54fea12464597fb33b203a50ffdca1
astute-macro-4903 <christopher.williams5699@gmail.com>
2020-08-27 00:09:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,7 +5,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  four_element_arr = ["chicken", "Fries", "Mashed Potatoes", "Sprite"]
+  four_element_arr_arr = ["chicken", "Fries", "Mashed Potatoes", "Sprite"]
   putsmpp four_element_arr
 end
 
